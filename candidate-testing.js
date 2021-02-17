6ae0af5557155727ce3147bc701ad089dd2c853a
McPeez08 <Peasallg@gmail.com>
2021-02-17 02:47:56 +0000

    uncommented all previously commented + deleted the original candidate question

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,9 +5,9 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-/*let question = "Who was the first American woman in space? ";
+let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
-let candidateAnswer;*/
+let candidateAnswer;
 let questions = ["1) Who was the first American woman in space? ", "2) True or false: 5000 meters = 5 kilometers. ", "3) (5+3)/2*10=? ", "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "5) What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
 let candidateAnswers = [];
@@ -25,23 +25,6 @@ console.log(`Candidate Name: ${candidateName}\n`);
 
 
 
-
-
-/*function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
-
-if (candidateAnswer.toUpperCase() === correctAnswer.toUpperCase()){
-
-  console.log(`That's correct, the answer is ${correctAnswer}!`);
-
-}else{
-
-  console.log(`I'm sorry, that's incorrect.`);
-
-}
-
-}*/
 
 
 
@@ -93,7 +76,7 @@ if(candidateAnswers[4].toUpperCase() === correctAnswers[4].toUpperCase()) {
     questionsCorrect += 1
   } 
 
-  /*// TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //*/
+  /*// TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
 
 
 let quizLength = questions.length
