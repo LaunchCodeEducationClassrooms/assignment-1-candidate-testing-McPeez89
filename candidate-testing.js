4e1f2b4e0a8e12507f58b1bbe86b12112a0ae1f6
McPeez08 <Peasallg@gmail.com>
2021-02-17 02:38:51 +0000

    I updated the variables to fit the required parametes, created a loop to ask the test questions and then scored the test and returned the candidate results.

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,34 +5,109 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
 let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["1) Who was the first American woman in space? ", "2) True or false: 5000 meters = 5 kilometers. ", "3) (5+3)/2*10=? ", "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "5) What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
+let candidateAnswers = [];
+let questionsCorrect = 0;
+
+
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+candidateName = input.question('What is your name? ');
+  
+console.log(`Candidate Name: ${candidateName}\n`);
+}
+
+
+
+
+
+/*function askQuestion() {
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+candidateAnswer = input.question(question);
+
+if (candidateAnswer.toUpperCase() === correctAnswer.toUpperCase()){
+
+  console.log(`That's correct, the answer is ${correctAnswer}!`);
+
+}else{
+
+  console.log(`I'm sorry, that's incorrect.`);
 
 }
+
+}*/
+
+
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
+let i = 0;
+
+while (i < questions.length) {
+
+ candidateAnswers.push(input.question(questions[i]));
+
+  console.log(`Your Answer: ${candidateAnswers[i]}`);
+  console.log(`Correct Answer: ${correctAnswers[i]}\n`);
+
+  i++;
+
+  }
 
 }
 
+
+
+
+
 function gradeQuiz(candidateAnswers) {
+
+  if(candidateAnswers[0].toUpperCase() === correctAnswers[0].toUpperCase()) {
+
+    questionsCorrect += 1
+  }
+
+ if(candidateAnswers[1].toUpperCase() === correctAnswers[1].toUpperCase()) {
+
+    questionsCorrect += 1
+  }
+
+ if(candidateAnswers[2].toUpperCase() === correctAnswers[2].toUpperCase()) {
+
+    questionsCorrect += 1
+  } 
+
+if(candidateAnswers[3].toUpperCase() === correctAnswers[3].toUpperCase()) {
+
+    questionsCorrect += 1
+  }
+
+if(candidateAnswers[4].toUpperCase() === correctAnswers[4].toUpperCase()) {
+
+    questionsCorrect += 1
+  } 
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
 
-  let grade;
+let quizLength = questions.length
+let grade = ((questionsCorrect) / (quizLength))*100
   
+  if(grade >= 80){
+    console.log(`>>> Overall Grade: ${grade}% (${questionsCorrect} of ${questions.length} responses correct) <<<\n>>> Status: PASSED <<< `)
+  }else{
+    console.log(`>>> Overall Grade: ${grade}% (${questionsCorrect} of ${questions.length} responses correct) <<<\n>>> Status: FAILED <<< `)
 
+  }
+  
   return grade;
+  
 }
 
 function runProgram() {
